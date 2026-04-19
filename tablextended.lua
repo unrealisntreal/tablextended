f71702cc643131a5e45ae40710eea5651b5fdfb8
Unreality <Unrealitydev@hotmail.com>
2026-04-19 18:28:52 +0200

    Update tablextended.lua

--- tablextended.lua
+++ tablextended.lua
@@ -1,18 +1,41 @@
----Sets the table keys to nil.
----@param t table
-function table.clear(t)
-  for k in next, t do
-    t[k] = nil
+local require = require
+local pairs = pairs
+local pcall = pcall
+
+do
+  local success, result = pcall(require, 'table.new')
+  table.fixed = (success and result) or function()
+    return { }
   end
 end
 
----Finds the table key of a value.
----@param t table
----@param value any
+do
+  local success, result = pcall(require, 'table.clear')
+  table.clear = (success and result) or function(t)
+    for k in pairs(t) do
+      t[k] = nil
+    end
+  end
+end
+
 function table.find(t, value)
   for i = #t, 1, -1 do
     if t[i] == value then
       return i
     end
   end
+end
+
+function table.clone(t, array)
+  local new = { }
+  if array then
+    for i = #t, 1, -1 do
+      new[i] = t[i]
+    end
+  else
+    for k, v in pairs(t) do
+      new[k] = v
+    end
+  end
+  return new
 end
